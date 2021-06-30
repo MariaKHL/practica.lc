15af26c4897b37547417dfd5b7ea2e5b086abac2
Ilia Glazunov <iglazunoff39@gmail.com>
2021-06-30 18:02:40 +0200

    Обновлен в качестве примера для экспериментов.

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Test</title>
+    <link rel="stylesheet" href="/app/dist/bundle.min.css">
+  </head>
+  <body>
+    <h1>It's Testpage!</h1>
+
+    <script src="/app/dist/bundle.min.js" charset="utf-8"></script>
+  </body>
+</html>
