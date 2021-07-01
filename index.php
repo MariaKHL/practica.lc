822a0a8ae83548427a12d6d66e5825918986be00
Ilia Glazunov <iglazunoff39@gmail.com>
2021-07-01 17:00:21 +0200

    Обновлен index.php

--- index.php
+++ index.php
@@ -1,13 +1,55 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Test</title>
-    <link rel="stylesheet" href="/app/dist/bundle.min.css">
-  </head>
-  <body>
-    <h1>It's Testpage!</h1>
+<?php
+/**
+* @package index
+* index.php - application entry point
+*
+* @version 1.0.0
+* @link https://github.com/MariaKHL/practica.lc.git
+* @copyright 2021 Khlystun Maria
+* @license MIT LICENSE Copyright (c) 2021 Khlystun Maria.
+* @NOTE: INNER DOC LANGUAGE IS RU;
+*/
 
-    <script src="/app/dist/bundle.min.js" charset="utf-8"></script>
-  </body>
-</html>
+
+  try {
+    /**
+    * @var string DSE Системный разделитель пути.
+    */
+    define('DSE', DIRECTORY_SEPARATOR);
+
+    /**
+    * @var string DIR_ROOT Абсолютный системный путь до корня проекта.
+    */
+    define('DIR_ROOT', __DIR__ . DSE);
+
+    /**
+    * @var string DIR_APP Абсолютный системный путь до корня проекта.
+    */
+    define('DIR_APP', DIR_ROOT . 'app' . DSE);
+
+    /**
+    * @var string FILE_MAIN Абсолютный системный путь до главного файла приложения.
+    */
+    define('FILE_MAIN', DIR_APP . 'main.php');
+
+    /**
+    * @var string CLASS_MAIN Имя класса главного файла приложения.
+    */
+    define('CLASS_MAIN', \App\Main::class);
+
+    if ( class_exists(CLASS_MAIN) && @is_file(FILE_MAIN) ) {
+      require_once FILE_MAIN;
+      $class = CLASS_MAIN;
+      $main = new $class();
+    }elseif (!class_exists(CLASS_MAIN)) {
+      throw new \Exception("Index: main class not found or was redefined!", 1);
+    }else {
+      throw new \Exception("Index: main file not found or path was redefined!", 1);
+    }
+  } catch (\Exception $e) {
+    print($e->getMessage());
+    exit;
+  }
+
+
+?>
